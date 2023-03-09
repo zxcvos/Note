96d08f5865c52799c1e45306c3022f0fad5a86b3
zxcvos <zxcvos@outlook.com>
2023-03-09 14:21:23 +0800

    add nginx_compile.sh

---
+++ proxy/nginx/nginx_compile.sh
@@ -0,0 +1,144 @@
+#!/usr/bin/env bash
+
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin:/snap/bin
+export PATH
+
+trap egress EXIT
+
+# 当前目录
+declare CUR_DIR="$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
+# 颜色
+declare RED='\033[1;31;31m'
+declare GREEN='\033[1;31;32m'
+declare YELLOW='\033[1;31;33m'
+declare NC='\033[0m'
+# 临时目录
+declare TMPFILE_DIR=$(mktemp -d -p ${CUR_DIR} -t nginx_compile.XXXXXXX) || exit 1
+
+function _info() {
+  printf "${GREEN}[Info] ${NC}"
+  printf -- "%s" "$1"
+  printf "\n"
+}
+
+function _warn() {
+  printf "${YELLOW}[Warning] ${NC}"
+  printf -- "%s" "$1"
+  printf "\n"
+}
+
+function _error() {
+  printf "${RED}[Error] ${NC}"
+  printf -- "%s" "$1"
+  printf "\n"
+  exit 1
+}
+
+function egress() {
+  [[ -e ${TMPFILE_DIR}/swap ]] && swapoff ${TMPFILE_DIR}/swap
+  rm -rf $TMPFILE_DIR
+}
+
+function install_dependence() {
+  apt update -y
+  apt install -y ca-certificates wget gcc g++ make perl-base perl libpcre2-dev zlib1g-dev libxml2-dev libxslt1-dev libgd-dev libgeoip-dev libgoogle-perftools-dev libperl-dev curl openssl cron
+}
+
+function get_latest_version() {
+  nginx_version="$(wget -qO- --no-check-certificate https://api.github.com/repos/nginx/nginx/tags | grep 'name' | cut -d\" -f4 | grep 'release' | head -1 | sed 's/release/nginx/')"
+  openssl_version="openssl-$(wget -qO- --no-check-certificate https://api.github.com/repos/openssl/openssl/tags | grep 'name' | cut -d\" -f4 | grep -Eoi '^openssl-([0-9]\.?){3}$' | head -1)"
+}
+
+function swap_on() {
+  local mem=${1}
+  if [[ ${mem} -ne '0' ]]; then
+    if dd if=/dev/zero of=${TMPFILE_DIR}/swap bs=1M count=${mem} 2>&1; then
+      chmod 0600 ${TMPFILE_DIR}/swap
+      mkswap ${TMPFILE_DIR}/swap
+      swapon ${TMPFILE_DIR}/swap
+    fi
+  fi
+}
+
+function gen_cflags() {
+  cflags=('-g0' '-O3')
+  if gcc -v --help 2>&1 | grep -qw "\\-fstack\\-reuse"; then
+    cflags+=('-fstack-reuse=all')
+  fi
+  if gcc -v --help 2>&1 | grep -qw "\\-fdwarf2\\-cfi\\-asm"; then
+    cflags+=('-fdwarf2-cfi-asm')
+  fi
+  if gcc -v --help 2>&1 | grep -qw "\\-fplt"; then
+    cflags+=('-fplt')
+  fi
+  if gcc -v --help 2>&1 | grep -qw "\\-ftrapv"; then
+    cflags+=('-fno-trapv')
+  fi
+  if gcc -v --help 2>&1 | grep -qw "\\-fexceptions"; then
+    cflags+=('-fno-exceptions')
+  elif gcc -v --help 2>&1 | grep -qw "\\-fhandle\\-exceptions"; then
+    cflags+=('-fno-handle-exceptions')
+  fi
+  if gcc -v --help 2>&1 | grep -qw "\\-funwind\\-tables"; then
+    cflags+=('-fno-unwind-tables')
+  fi
+  if gcc -v --help 2>&1 | grep -qw "\\-fasynchronous\\-unwind\\-tables"; then
+    cflags+=('-fno-asynchronous-unwind-tables')
+  fi
+  if gcc -v --help 2>&1 | grep -qw "\\-fstack\\-check"; then
+    cflags+=('-fno-stack-check')
+  fi
+  if gcc -v --help 2>&1 | grep -qw "\\-fstack\\-clash\\-protection"; then
+    cflags+=('-fno-stack-clash-protection')
+  fi
+  if gcc -v --help 2>&1 | grep -qw "\\-fstack\\-protector"; then
+    cflags+=('-fno-stack-protector')
+  fi
+  if gcc -v --help 2>&1 | grep -qw "\\-fcf\\-protection="; then
+    cflags+=('-fcf-protection=none')
+  fi
+  if gcc -v --help 2>&1 | grep -qw "\\-fsplit\\-stack"; then
+    cflags+=('-fno-split-stack')
+  fi
+  if gcc -v --help 2>&1 | grep -qw "\\-fsanitize"; then
+    >temp.c
+    if gcc -E -fno-sanitize=all temp.c >/dev/null 2>&1; then
+      cflags+=('-fno-sanitize=all')
+    fi
+    rm temp.c
+  fi
+  if gcc -v --help 2>&1 | grep -qw "\\-finstrument\\-functions"; then
+    cflags+=('-fno-instrument-functions')
+  fi
+}
+
+function compile_nginx() {
+  _info "正在编译Nginx。。。。"
+  get_latest_version
+  gen_cflags
+  if ! wget -O ${nginx_version}.tar.gz https://nginx.org/download/${nginx_version}.tar.gz; then
+    _error "获取nginx失败"
+  fi
+  tar -zxf ${nginx_version}.tar.gz
+  rm -f "${nginx_version}.tar.gz"
+  if ! wget -O ${openssl_version}.tar.gz https://github.com/openssl/openssl/archive/${openssl_version#*-}.tar.gz; then
+    _error "获取openssl失败"
+  fi
+  tar -zxf ${openssl_version}.tar.gz
+  rm -f "${openssl_version}.tar.gz"
+  cd ${nginx_version}
+  sed -i "s/OPTIMIZE[ \\t]*=>[ \\t]*'-O'/OPTIMIZE          => '-O3'/g" src/http/modules/perl/Makefile.PL
+  sed -i 's/NGX_PERL_CFLAGS="$CFLAGS `$NGX_PERL -MExtUtils::Embed -e ccopts`"/NGX_PERL_CFLAGS="`$NGX_PERL -MExtUtils::Embed -e ccopts` $CFLAGS"/g' auto/lib/perl/conf
+  sed -i 's/NGX_PM_CFLAGS=`$NGX_PERL -MExtUtils::Embed -e ccopts`/NGX_PM_CFLAGS="`$NGX_PERL -MExtUtils::Embed -e ccopts` $CFLAGS"/g' auto/lib/perl/conf
+  ./configure --prefix="${nginx_prefix}" --user=root --group=root --with-threads --with-file-aio --with-http_ssl_module --with-http_v2_module --with-http_realip_module --with-http_addition_module --with-http_xslt_module=dynamic --with-http_image_filter_module=dynamic --with-http_geoip_module=dynamic --with-http_sub_module --with-http_dav_module --with-http_flv_module --with-http_mp4_module --with-http_gunzip_module --with-http_gzip_static_module --with-http_auth_request_module --with-http_random_index_module --with-http_secure_link_module --with-http_degradation_module --with-http_slice_module --with-http_stub_status_module --with-http_perl_module=dynamic --with-mail=dynamic --with-mail_ssl_module --with-stream=dynamic --with-stream_ssl_module --with-stream_realip_module --with-stream_geoip_module=dynamic --with-stream_ssl_preread_module --with-google_perftools_module --with-compat --with-cc-opt="${cflags[*]}" --with-openssl="../${openssl_version}" --with-openssl-opt="${cflags[*]}"
+  swap_on 512
+  if ! make -j$(nproc); then
+    _error "Nginx编译失败！"
+  fi
+  _info "正在安装Nginx。。。"
+  make install
+}
+
+cd ${TMPFILE_DIR}
+install_dependence
+compile_nginx
