43bd0355e2af10bbbf1787dc5249091815d183c6
zxcvos <78369539+zxcvos@users.noreply.github.com>
2023-03-26 14:18:04 +0800

    Update check_tls13_h2_support.sh

--- proxy/tools/check_tls13_h2_support.sh
+++ proxy/tools/check_tls13_h2_support.sh
@@ -6,7 +6,7 @@ if [[ -z "$1" ]]; then
 fi
 
 CHECK_URL=$(echo $1 | grep -oE '[^/]+(\.[^/]+)+\b' | head -n 1)
-CHECK_NUM=$(echo QUIT | stdbuf -oL openssl s_client -connect "${check_url}:443" -tls1_3 -alpn h2 2>&1 | grep -Eoi '(TLSv1.3)|(^ALPN\s+protocol:\s+h2$)|(X25519)' | sort -u | wc -l)
+CHECK_NUM=$(echo QUIT | stdbuf -oL openssl s_client -connect "${CHECK_URL}:443" -tls1_3 -alpn h2 2>&1 | grep -Eoi '(TLSv1.3)|(^ALPN\s+protocol:\s+h2$)|(X25519)' | sort -u | wc -l)
 
 if [[ ${CHECK_NUM} -eq 3 ]]; then
   echo "https://${CHECK_URL} supports TLS 1.3 and h2"
